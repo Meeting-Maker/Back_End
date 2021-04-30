bc06a7d87c0392a30d95575efea0b3bd94743138
MingHaoC <chen1fl@uwindsor.ca>
2021-04-30 17:40:22 -0400

    [#3] finish user registration and login endpoints, already tested with postman, need to test with frontend

--- src/models/User.js
+++ src/models/User.js
@@ -1,26 +1,51 @@
-module.exports = (sequelize, DataTypes) => {
-   const User = sequelize.define('User', {
-      id :  {
-         type: DataTypes.INTEGER,
-         autoIncrement: true,
-         allowNull: false,
-         primaryKey: true
-      },
-      name: {
-         type: DataTypes.STRING(40),
-         allowNull: false
-      },
-      email: {
-        type: DataTypes.STRING(254)
-      },
-      password: {
-         type: DataTypes.STRING(60)
-      },
-      guest: {
-         type: DataTypes.TINYINT(),
-         defaultValue: 1
-      },
-   });
+const Promise = require('bluebird');
+const bcrypt = Promise.promisifyAll(require('bcryptjs'));
 
-   return User;
+function hashPassword(user) {
+    const SALT_Factor = 8;
+
+    if (!user.changed('password'))
+        return;
+
+    return bcrypt.hash(user.password, SALT_Factor, null)
+        .then(hash => {
+            user.setDataValue('password', hash)
+        });
+
+}
+
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define('User', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey: true
+        },
+        name: {
+            type: DataTypes.STRING(40),
+            allowNull: false
+        },
+        email: {
+            type: DataTypes.STRING(254)
+        },
+        password: {
+            type: DataTypes.STRING(60)
+        },
+        guest: {
+            type: DataTypes.TINYINT(),
+            defaultValue: 1
+        }
+    }, {
+        hooks: {
+            beforeSave: hashPassword
+        }
+    });
+
+    User.prototype.comparePassword = function (password) {
+        return bcrypt.compare(password, this.password);
+    };
+
+
+    return User;
 }
